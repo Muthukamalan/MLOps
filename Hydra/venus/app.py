645f0f743b393722352a29274608adb423f72e67
muthukamalan <muthukamalan98@gmail.com>
2024-09-28 23:28:54 +0530

    added loguru eg

--- Hydra/venus/app.py
+++ Hydra/venus/app.py
@@ -2,10 +2,26 @@ from omegaconf import OmegaConf,DictConfig
 import hydra 
 from hydra.core.hydra_config import HydraConfig
 
+from loguru import logger
+import sys 
+
+logger.add(
+    sink='info.log', #  sys.stdout,
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {module}:{function}:{line} - {message}"  ,
+    level = "DEBUG",
+    rotation="1 week",
+    retention="4 weeks"
+)
+
 
 @hydra.main(version_base=None, config_name="config",config_path="conf")
 def main(cfg:DictConfig)->None:
     print(HydraConfig.get().job.name)
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warning")
+    logger.error("error")
+
     print(HydraConfig.get().runtime.version)
     print(HydraConfig.get().runtime.cwd) #${hydra:runtime.cwd}
     print(OmegaConf.to_yaml(cfg))
